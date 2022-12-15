2bd5b827222ca609c8df1950ea0310d7d4fa5973
Kathleen Jumamoy <kathleen.jumamoy@Kathleens-MacBook-Pro.local>
2022-12-14 20:21:00 -0700

    added constructor to controller

--- shredders-hub-service/Controllers/ItemsSwapController.cs
+++ shredders-hub-service/Controllers/ItemsSwapController.cs
@@ -2,24 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 using shredders_hub_application;
 using shredders_hub_application.Models;
 
-namespace shredders_hub_service.Controllers;
-
-[Route("items-swap")]
-[ApiController]
-
-public class ItemsSwapController
+namespace shredders_hub_service.Controllers
 {
-    private IShreddersHubRepository _shreddersHubRepository;
-    [HttpGet]
-    public Task<List<ItemInfo>> GetItemDetails()
-    {
-        return _shreddersHubRepository.GetAll();
-    }
+    [Route("items-swap")]
+    [ApiController]
 
-    [HttpPost]
-    public Task AddItemDetails(ItemInfo itemName)
+    public class ItemsSwapController
     {
-        return _shreddersHubRepository.Add(itemName);
+        private IShreddersHubRepository _shreddersHubRepository;
+
+        public ItemsSwapController(IShreddersHubRepository shreddersHubRepository)
+        {
+            _shreddersHubRepository = shreddersHubRepository;
+        }
+
+        [HttpGet]
+        public Task<List<ItemInfo>> GetItemDetails()
+        {
+            return _shreddersHubRepository.GetAll();
+        }
+
+        [HttpPost]
+        public Task AddItemDetails(ItemInfo itemName)
+        {
+            return _shreddersHubRepository.Add(itemName);
+        }
     }
-    
 }
